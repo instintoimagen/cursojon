cbc1769a326cbcb25cf558df4a87cd81a101a9ce
Sergio <instintoimagen@gmail.com>
2022-05-05 15:20:04 -0300

    syntaxis jsx v.3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let nombre = "Sergio";
+  let profesiones = [
+    "Desarrollador",
+    "Director de Tv",
+    "Fotógrafo",
+    "Papá de Joaquín",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{nombre}</h1>
+          <ul>
+            {profesiones.map((el, index) => (
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+          <p>
+            Editalo <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
